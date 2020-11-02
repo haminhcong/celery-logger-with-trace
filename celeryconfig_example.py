4778b34422e04f8de2e935ce07fe422d9d2b6ac0
conghm96 <conghm96@gmail.com>
2020-11-03 00:32:27 +0700

    update config example

--- celeryconfig_example.py
+++ celeryconfig_example.py
@@ -1,4 +1,4 @@
-broker_url = "redis://:H733Kdjndks81@127.0.0.1:6379/0"
+broker_url = "redis://:PASSWORD@127.0.0.1:6379/0"
 
 # # broker_url = "redis://:H733Kdjndks81@192.168.0.101:6379/0"
 # broker_transport_options = {'master_name': "mymaster"}
